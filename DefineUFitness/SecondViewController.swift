cfb42c496efcb9967825028ddfe9e6392a2a40a3
Hsanka6 <haasith2@gmail.com>
2019-06-30 12:06:52 -0700

    Added a map for events

--- DefineUFitness/SecondViewController.swift
+++ DefineUFitness/SecondViewController.swift
@@ -8,15 +8,60 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class SecondViewController: UIViewController {
-    @IBOutlet var mapKit: MKMapView!
+class SecondViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet var mapView: MKMapView!
+    let regionRadius: CLLocationDistance = 1000
+   
+    
+    var locationManager: CLLocationManager!
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                                  latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationSetup()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func locationSetup(){
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+    }
 
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let locValue:CLLocationCoordinate2D = manager.location?.coordinate{
+//            self.lat = locValue.latitude
+//            self.lon = locValue.longitude
+//
+            let initialLocation = CLLocation(latitude: locValue.latitude, longitude: locValue.longitude)
+            
+            centerMapOnLocation(location: initialLocation)
+
+           // print("lat is \(locValue.latitude) and lon is \(locValue.longitude)")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
+    {
+        print("Location Error \(error)")
+    }
+    
 
 }
 
