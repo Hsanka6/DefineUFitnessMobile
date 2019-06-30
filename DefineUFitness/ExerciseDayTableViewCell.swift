811d2467b735eb018f6bd2498ff087a0d2411f2d
Hsanka6 <haasith2@gmail.com>
2019-06-30 11:51:35 -0700

    Set up exercise table

--- DefineUFitness/ExerciseDayTableViewCell.swift
+++ DefineUFitness/ExerciseDayTableViewCell.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ExerciseDayTableViewCell: UITableViewCell {
-
+    @IBOutlet var dateLabel: UILabel!
+    
+    @IBOutlet var exerciseList: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    
+    func setUp(date: String, exercises: String){
+        dateLabel.text = date
+        exerciseList.text = exercises
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- DefineUFitness/FirstViewController.swift
+++ DefineUFitness/FirstViewController.swift
@@ -8,10 +8,33 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var dates = ["1/2/22","1/3/22"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dates.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "exerciseCell") as? ExerciseDayTableViewCell else{
+            return UITableViewCell()
+        }
+        cell.setUp(date: dates[indexPath.row], exercises: "3x Situps")
+        return cell
+    }
+    
 
+    @IBOutlet var tableView: UITableView!
+    func setUpTable(){
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpTable()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
