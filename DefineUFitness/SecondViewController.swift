06e5d4e5f1ba1ad6bc2c97ff01f672909a57ecf5
Hsanka6 <haasith2@gmail.com>
2019-06-30 11:09:20 -0700

    Initial Commit

---
+++ DefineUFitness/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  DefineUFitness
+//
+//  Created by Haasith Sanka on 6/30/19.
+//  Copyright © 2019 Haasith Sanka. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
