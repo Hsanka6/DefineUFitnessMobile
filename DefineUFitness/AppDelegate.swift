44fa7e49b1672f7c301eab48bfaf265b60c524e9
Hsanka6 <haasith2@gmail.com>
2019-07-03 13:45:45 -0700

    Added firebase and google sign in pods

--- DefineUFitness/AppDelegate.swift
+++ DefineUFitness/AppDelegate.swift
@@ -7,15 +7,16 @@
 //
 
 import UIKit
-
+import Firebase
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
